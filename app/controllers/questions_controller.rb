565c312d56f7941787ffec742c94b63f7c1e4ab4
Kota Watanabe <kota3316@gmail.com>
2019-04-10 13:19:15 -0700

    refactor controller new and edit page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,70 @@
+class QuestionsController < ApplicationController
+    before_action :find_question, only: [:show, :edit, :update, :destroy]
+  
+    def new
+      @question = Question.new
+    end
+  
+    def create
+      # # The 'params' object available in controllers
+      # # is composed of the query string, url params,
+      # # and the body of a form
+      # # (e.g. req.query + req.params + req.body)
+      #
+      # # A good trick to see if your routes are working
+      # # and you're getting the data that you want, is to
+      # # render the params as JSON. This is like doing
+      # # res.send(req.body) in Express
+      @question = Question.new question_params
+      if @question.save
+        # The redirect_to method is used for telling the
+        # browser to make a new request.
+        # The redirect_to method is typically used with
+        # with a named route helper.
+        redirect_to question_path(@question.id)
+      else
+        render :new
+      end
+    end
+  
+    def show
+    end
+  
+    def index
+      @questions = Question.all.order(created_at: :desc)
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @question.update question_params
+        redirect_to question_path(@question.id)
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @question.destroy
+      redirect_to root_path
+    end
+  
+    private
+  
+    def question_params
+      # Use 'require' on the params object to retrieve
+      # the nested hash of a key usually corresponding to
+      # the name-value pairs of a form
+  
+      # Then use permit to specify all input names that
+      # are allowable (as symbols).
+      params.require(:question).permit(:title, :body)
+    end
+  
+    def find_question
+      @question = Question.find(params[:id])
+    end
+  
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   post('/contacts', { to: 'contact#create' })
   get("/contacts/new", {to: "contact#new"})
+  get('/questions/new', {to: 'questions#new', as: :new_question})
+  post('/questions', to: 'questions#create', as: :questions)
+  get('/questions/:id', to: 'questions#show', as: :question)
+  get('/questions/:id/edit', to: 'questions#edit', as: :edit_question)
+  patch("/questions/:id", to: "questions#update")
+  delete('/questions/:id', to: 'questions#destroy')
+  get('/questions', to: 'questions#index')
   # this defines a `route` rule that says when we recieve a `GET` request with 
   # URL `/`, handle it using the `WelcomeController` with `index` action 
   # inside that controller
   # the `as` option is used for helper url/path
   get('/', { to: 'welcome#index', as: 'root' })
+
 end
